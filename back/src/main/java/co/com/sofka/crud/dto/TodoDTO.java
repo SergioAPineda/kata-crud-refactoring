c4bcf4555b88ee884e224302125a051553c90c60
Sergio Pineda <sergio.pinedas94@gmail.com>
2021-06-03 15:24:50 -0500

    TodoDTO and TodoDTOResponse have been created. All java files were organized in packages

---
+++ back/src/main/java/co/com/sofka/crud/dto/TodoDTO.java
@@ -0,0 +1,52 @@
+package co.com.sofka.crud.dto;
+
+import java.io.Serializable;
+
+public class TodoDTO implements Serializable {
+    private Long id;
+    private String name;
+    private boolean completed;
+    private String groupListId;
+
+    public TodoDTO() {
+    }
+
+    public TodoDTO(Long id, String name, boolean completed, String groupListId) {
+        this.id = id;
+        this.name = name;
+        this.completed = completed;
+        this.groupListId = groupListId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public String getGroupListId() {
+        return groupListId;
+    }
+
+    public void setGroupListId(String groupListId) {
+        this.groupListId = groupListId;
+    }
+}
---
+++ back/src/main/java/co/com/sofka/crud/dto/TodoDTOResponse.java
@@ -0,0 +1,17 @@
+package co.com.sofka.crud.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoDTOResponse {
+    List<TodoDTO> todoListDTO;
+
+    public TodoDTOResponse() {
+        super();
+        todoListDTO = new ArrayList<TodoDTO>();
+    }
+
+    public List<TodoDTO> getListDTO() { return todoListDTO; }
+    public void setListDTO(List<TodoDTO> todoListDTO){ this.todoListDTO = todoListDTO; }
+    public void addListDTO(TodoDTO todoDTO){ this.todoListDTO.add(todoDTO); }
+}
